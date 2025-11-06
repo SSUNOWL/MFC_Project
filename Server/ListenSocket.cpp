17d3c58fd7d5da8102a2230722061b354081f5a6
Sunjae <sunj0321@naver.com>
2025-11-06 14:43:52 +0900

    user-serverip

--- Server/ListenSocket.cpp
+++ Server/ListenSocket.cpp
@@ -15,13 +15,17 @@ CListenSocket::~CListenSocket()
 
 void CListenSocket::OnAccept(int nErrorCode)
 {
-    if (nErrorCode == 0 && m_pServerDlg)
+    CAsyncSocket::OnAccept(nErrorCode);
+
+        // TODO: ServerDlg에 pNewSocket 객체를 관리 리스트에 추가하는 로직 필요
+        // m_pServerDlg->AddServiceSocket(pNewSocket);
+    if(nErrorCode == 0 && m_pServerDlg)
     {
         //  연결 수락 처리를 대화 상자에게 위임합니다.
         // CServerDlg에 ProcessAccept 함수를 구현해야 합니다.
+
         m_pServerDlg->ProcessAccept(this);
+
     }
-
-    CAsyncSocket::OnAccept(nErrorCode);
+ 
 }
-
