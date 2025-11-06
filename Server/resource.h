da5609de15204cc7e5cdf2f0d3859768dedef17b
Jungbin906 <jungbin906@naver.com>
2025-11-06 11:42:37 +0900

    Update resource.h

--- Server/resource.h
+++ Server/resource.h
@@ -13,6 +13,9 @@
 #define IDC_LIST_MESSAGE                1002
 #define IDC_LIST_LOG                    1003
 #define IDC_BUTTON_START                1004
+#define IDC_BUTTON_PASS                 1005
+#define IDC_BUTTON_SetBack              1006
+#define IDC_BUTTON_RECEIVE              1007
 
 // Next default values for new objects
 // 
@@ -20,7 +23,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
