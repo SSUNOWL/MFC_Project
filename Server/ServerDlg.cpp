68660b8e4cb9bd027471e296230000506338cd44
Jungbin906 <jungbin906@naver.com>
2025-11-14 10:06:56 +0900

    Update ServerDlg.cpp

--- Server/ServerDlg.cpp
+++ Server/ServerDlg.cpp
@@ -481,7 +481,7 @@ void CServerDlg::OnBnClickedButtonReceive()
 {
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
 	// 시작 전 판을 변경 전으로 돌리는 SetBack 실행
-	//CString strMsg;
-	//strMsg.Format(_T("type:RECEIVE|sender:%s"), m_strName);
-	//ResponseMessage(strMsg,); // 이건 client에서
+	CString strMsg;
+	strMsg.Format(_T("type:RECEIVE|sender:%s"), m_strName);
+	ResponseMessage(strMsg,); // 이건 client에서
 }
