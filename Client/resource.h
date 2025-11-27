105c05de7508d7d40cdfb991c3224a0fc297a96d
Sunjae <sunj0321@naver.com>
2025-11-27 16:50:41 +0900

    update: playerlist

--- Client/resource.h
+++ Client/resource.h
@@ -74,6 +74,7 @@
 #define IDC_BUTTON_RECEIVE3             1009
 #define IDC_BUTTON_SETBACK              1009
 #define IDC_IPADDRESS_IP                1010
+#define IDC_LIST_PLAYER                 1011
 
 // Next default values for new objects
 // 
@@ -81,7 +82,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        188
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
--- Server/resource.h
+++ Server/resource.h
@@ -75,6 +75,8 @@
 #define IDC_IPADDRESS_IP                1007
 #define IDC_BUTTON_SetBack2             1008
 #define IDC_BUTTON_PLAY                 1008
+#define IDC_LIST1                       1009
+#define IDC_LIST_PLAYER                 1009
 
 // Next default values for new objects
 // 
@@ -82,7 +84,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        299
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
